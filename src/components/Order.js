ea5cf5ea3a8719d9eef225b68761c6b8c0c12895
Nuo (Tony) Chen <chen2886@purdue.edu>
2021-10-26 13:09:59 -0400

    Added orders section

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,64 +1,120 @@
 import React, { useEffect } from "react";
 import OrderCard from "./OrderCard";
 import axiosClient from "../utils/axiosClient";
-import { Button, Grid, Icon, IconButton } from "@mui/material";
+import {
+  Grid,
+  IconButton,
+  Accordion,
+  Typography,
+  AccordionDetails,
+  AccordionSummary,
+  CircularProgress,
+} from "@mui/material";
 import styled from "styled-components";
 import { AddCircle } from "@mui/icons-material";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+
+const StyledGrid = styled(Grid)`
+  width: 100%;
+  margin: 0;
+  box-sizing: border-box;
+`;
+
+const StyledGridItem = styled(Grid)`
+  padding: 1rem;
+`;
+
+const AddOrderGridItem = styled(Grid)`
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+`;
+
+const StyledGridWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  box-sizing: border-box;
+  padding-top: 3rem;
+`;
+
+const StyledAddButton = styled(IconButton)`
+  font-size: 50px;
+`;
+
+const StyledExpandWrapper = styled.div`
+  margin: 5rem;
+`;
 
 export default function Order() {
-  const [orders, setOrders] = React.useState([]);
-
-  const StyledGrid = styled(Grid)`
-    width: 100%;
-    margin: 0;
-    box-sizing: border-box;
-  `;
-
-  const StyledGridItem = styled(Grid)`
-    padding: 1rem;
-  `;
-
-  const AddOrderGridItem = styled(Grid)`
-    padding: 1rem;
-    display: flex;
-    justify-content: center;
-  `;
-
-  const StyledGridWrapper = styled.div`
-    display: flex;
-    justify-content: center;
-    box-sizing: border-box;
-    padding-left: 10%;
-    padding-right: 10%;
-    padding-top: 3rem;
-  `;
-
-  const StyledAddButton = styled(IconButton)`
-    font-size: 50px;
-  `;
+  const [orders, setOrders] = React.useState({});
+  const [expand, setExpand] = React.useState([true, false]);
+  const [loading, setLoading] = React.useState(true);
 
   // useEffect with empty array runs when components mount
   useEffect(() => {
     // use axiosClient created to get all orders
     axiosClient.get("/orders").then((res) => {
       setOrders(res.data);
+      setLoading(false);
     });
   }, []);
 
+  const handleExpand = (index) => {
+    let arr = expand.slice();
+    arr[index] = !arr[index];
+    setExpand(arr);
+  };
+
   return (
-    <StyledGridWrapper>
-      <StyledGrid container justifyContent='center' alignItems='center'>
-        {orders.map((item) => (
-          <StyledGridItem item xs={6} key={item.Order_ID}>
-            <OrderCard info={item}></OrderCard>
-          </StyledGridItem>
-        ))}
-        <AddOrderGridItem item xs={12}>
-          <StyledAddButton>
-            <AddCircle fontSize='inherit'></AddCircle>
-          </StyledAddButton>
-        </AddOrderGridItem>
-      </StyledGrid>
-    </StyledGridWrapper>
+    <StyledExpandWrapper>
+      <Accordion expanded={expand[0]} onChange={() => handleExpand(0)}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>Current Order</Typography>
+        </AccordionSummary>
+        {!loading && (
+          <AccordionDetails>
+            <StyledGridWrapper>
+              <StyledGrid container justifyContent='center' alignItems='center'>
+                {orders.Current.map((item) => (
+                  <StyledGridItem item xs={4} key={item.Order_ID}>
+                    <OrderCard info={item}></OrderCard>
+                  </StyledGridItem>
+                ))}
+                <AddOrderGridItem item xs={12}>
+                  <StyledAddButton>
+                    <AddCircle fontSize='inherit'></AddCircle>
+                  </StyledAddButton>
+                </AddOrderGridItem>
+              </StyledGrid>
+            </StyledGridWrapper>
+          </AccordionDetails>
+        )}
+        {loading && <CircularProgress></CircularProgress>}
+      </Accordion>
+      <Accordion expanded={expand[1]} onChange={() => handleExpand(1)}>
+        <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+          <Typography>Completed Order</Typography>
+        </AccordionSummary>
+        {!loading && (
+          <AccordionDetails>
+            <StyledGridWrapper>
+              <StyledGrid container justifyContent='center' alignItems='center'>
+                {orders.Completed.map((item) => (
+                  <StyledGridItem item xs={4} key={item.Order_ID}>
+                    <OrderCard info={item}></OrderCard>
+                  </StyledGridItem>
+                ))}
+                <AddOrderGridItem item xs={12}>
+                  <StyledAddButton>
+                    <AddCircle fontSize='inherit'></AddCircle>
+                  </StyledAddButton>
+                </AddOrderGridItem>
+              </StyledGrid>
+            </StyledGridWrapper>
+          </AccordionDetails>
+        )}
+        {loading && <CircularProgress></CircularProgress>}
+      </Accordion>
+    </StyledExpandWrapper>
   );
 }
--- src/components/OrderCard.js
+++ src/components/OrderCard.js
@@ -2,36 +2,86 @@ import React from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
-import Avatar from "@mui/material/Avatar";
+import axiosClient from "../utils/axiosClient";
 import Typography from "@mui/material/Typography";
+import CheckIcon from "@mui/icons-material/Check";
 import moment from "moment";
+import styled from "styled-components";
+import { Divider, Grid, CardActions, IconButton } from "@mui/material";
+import "../styles/Order.css";
+
+let DateDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+  flex-wrap: wrap;
+  text-align: center;
+`;
+
+let DividerWrapper = styled(Grid)`
+  display: flex;
+  justify-content: center;
+`;
 
 export default function OrderCard(props) {
   let info = props.info;
-  let fn = info.First_Name.charAt(0);
-  let ln = info.Last_Name === undefined ? "" : info.Last_Name.charAt(0);
-  let date = moment(info.Date).format("MM/DD/YYYY hh:mm:ss A");
+  let fn = info.First_Name === null ? "" : info.First_Name;
+  let ln = info.Last_Name === null ? "" : info.Last_Name;
+  let d = new Date(info.Date);
+  let date = moment(d).format("MM/DD/YYYY");
+  let time = moment(d).format("hh:mm A");
+
+  const completeOrder = (Order_ID) => {
+    console.log(Order_ID);
+    axiosClient
+      .post("/complete", {
+        Order_ID,
+      })
+      .then((res) => {
+        console.log(res);
+      });
+  };
 
   return (
     <Card>
-      <CardHeader
-        avatar={<Avatar aria-label='recipe'>{fn + ln}</Avatar>}
-        title={info.Name}
-        subheader={date}
-      />
+      <CardHeader title={info.Name} subheader={"$" + info.Price} />
+      <Divider></Divider>
       <CardContent>
-        <Typography variant='body2' color='text.secondary'>
-          {info.Name}
-        </Typography>
+        <Grid container direction='row' justifyContent='center'>
+          <Grid item xs={5} className='info-grid-item'>
+            <Typography
+              variant='caption'
+              color='text.secondary'
+              style={{ textAlign: "center" }}>
+              Customer
+            </Typography>
+            <Typography variant='h6' style={{ textAlign: "center" }}>
+              {fn.length > 1 && ln.length > 1 && fn + " " + ln}
+              {fn.length === 0 && ln.length === 0 && "Anonymous"}
+            </Typography>
+          </Grid>
+          <DividerWrapper item xs={2}>
+            <Divider orientation='vertical'></Divider>
+          </DividerWrapper>
+          <Grid item xs={5} className='info-grid-item'>
+            <Typography
+              variant='caption'
+              color='text.secondary'
+              style={{ textAlign: "center" }}>
+              Time
+            </Typography>
+            <DateDiv>
+              <Typography variant='h6'>{date}</Typography>
+              <Typography variant='h6'>{time}</Typography>
+            </DateDiv>
+          </Grid>
+        </Grid>
       </CardContent>
-      {/* <CardActions disableSpacing>
-        <IconButton aria-label='add to favorites'>
-          <FavoriteIcon />
+      <CardActions disableSpacing className='card-actions'>
+        <IconButton onClick={() => completeOrder(info.Order_ID)}>
+          <CheckIcon />
         </IconButton>
-        <IconButton aria-label='share'>
-          <ShareIcon />
-        </IconButton>
-      </CardActions> */}
+      </CardActions>
     </Card>
   );
 }
