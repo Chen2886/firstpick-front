a5f9a3352d746f1bec6cf2a9d2dfc884896f0ea8
Iris Yuan <irisjyuan@gmail.com>
2021-11-01 17:10:01 -0400

    display customer table

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -9,41 +9,28 @@ import { DataGrid } from '@material-ui/data-grid'
 // show the customers in table form
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'firstName', headerName: 'First name', width: 200 },
-  { field: 'lastName', headerName: 'Last name', width: 200 },
+  { field: 'First_Name', headerName: 'First name', width: 200 },
+  { field: 'Last_Name', headerName: 'Last name', width: 200 },
   {
-    field: 'age',
+    field: 'Age',
     headerName: 'Age',
     type: 'number',
     width: 150,
   },
-  { field: 'email', headerName: 'Email', width: 200 },
-  { field: 'phoneNumber', headerName: 'Phone number', width: 200 },
-];
-
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { field: 'Email', headerName: 'Email', width: 200 },
+  { field: 'Phone_Number', headerName: 'Phone number', width: 200 },
 ];
 
 export default function Customer() {
 
-  const [customer, addCustomer] = React.useState({});
+  const [customer, addCustomer] = React.useState([]);
   //const [customer, deleteCustomer] = React.useState({});
 
   // gets all the customers from backend the first time the component loads
   useEffect(() => {
     // use axiosClient created to get all customers
     axiosClient.get("/customers").then((res) => {
-      addCustomer(res.data.Current);
-      // setCompletedOrder(res.data.Completed);
-      // setLoading(false);
+      addCustomer(res.data);
     });
   }, []);
 
@@ -51,11 +38,10 @@ export default function Customer() {
   return (
     <div style={{ height: 800, width: '100%' }}>
       <DataGrid
-        rows={rows}
+        rows={customer}
         columns={columns}
         pageSize={100}
         rowsPerPageOptions={[100]}
-        checkboxSelection
       />
     </div>
   );
