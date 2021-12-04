37fbdd11d9ffb9e3398b3121e5f554dd9c4623ad
Nuo (Tony) Chen <chen2886@purdue.edu>
2021-12-04 18:30:15 -0500

    Changed customers to table

--- src/components/Customer.js
+++ src/components/Customer.js
@@ -9,11 +9,17 @@ import {
   TableCell,
   Paper,
   TableBody,
+  Grid,
 } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import CustomerAddDialog from "./CustomerAddDialog";
+import styled from "styled-components";
 
 const col = ["First Name", "Last Name", "Age", "Email", "Phone Number"];
+
+const StyledGridContainer = styled(Grid)`
+  margin-top: 1rem;
+`;
 
 export default function Customer() {
   const [customer, addCustomer] = React.useState([]);
@@ -31,33 +37,46 @@ export default function Customer() {
         key={openAddDialog}
         openAddDialog={openAddDialog}
         setOpenAddDialog={setOpenAddDialog}></CustomerAddDialog>
-      <TableContainer component={Paper}>
-        <Table stickyHeader size='medium'>
-          <TableHead>
-            <TableRow>
-              {col.map((item) => (
-                <TableCell>{item}</TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {customer.map((row) => (
-              <TableRow
-                key={row.id}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                <TableCell>{row.First_Name}</TableCell>
-                <TableCell>{row.Last_Name}</TableCell>
-                <TableCell>{row.Age}</TableCell>
-                <TableCell>{row.Email}</TableCell>
-                <TableCell>{row.Phone_Number}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <Fab onClick={() => setOpenAddDialog(true)} color='primary'>
-        <AddIcon />
-      </Fab>
+      <StyledGridContainer container>
+        <Grid item xs={2}></Grid>
+        <Grid item xs={8}>
+          <TableContainer component={Paper}>
+            <Table stickyHeader size='medium'>
+              <TableHead>
+                <TableRow>
+                  {col.map((item) => (
+                    <TableCell>{item}</TableCell>
+                  ))}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {customer.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
+                    <TableCell>{row.First_Name}</TableCell>
+                    <TableCell>{row.Last_Name}</TableCell>
+                    <TableCell>{row.Age}</TableCell>
+                    <TableCell>{row.Email}</TableCell>
+                    <TableCell>{row.Phone_Number}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Grid>
+        <Grid
+          item
+          xs={2}
+          style={{ display: "flex", justifyContent: "flex-end" }}>
+          <Fab
+            onClick={() => setOpenAddDialog(true)}
+            color='primary'
+            style={{ marginRight: "1rem" }}>
+            <AddIcon />
+          </Fab>
+        </Grid>
+      </StyledGridContainer>
     </div>
   );
 }
